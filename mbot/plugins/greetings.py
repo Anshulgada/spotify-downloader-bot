d37e33fc09fcdd5239dc5d80a1179ff9757faab2
Anshulgada <80207612+Anshulgada@users.noreply.github.com>
2022-09-21 00:43:19 +0530

    Updates

--- mbot/plugins/greetings.py
+++ mbot/plugins/greetings.py
@@ -109,7 +109,7 @@ async def help(_, message):
     ]
 
     await message.reply_text(
-        f"Hello **{message.from_user.first_name}**, I'm **@NeedMusicRobot**.\nI'm Here to download your music.",
+        f"Hello **{message.from_user.first_name}**, I'm **@MagSpotifyBot**.\nI'm Here to download your music.",
         reply_markup=InlineKeyboardMarkup(button),
     )
 
@@ -130,6 +130,6 @@ async def help_home(_, query):
         [InlineKeyboardButton(text=i, callback_data=f"help_{i}")] for i in HELP
     ]
     await query.message.edit(
-        f"Hello **{query.from_user.first_name}**, I'm **@NeedMusicRobot**.\nI'm Here to download your music.",
+        f"Hello **{query.from_user.first_name}**, I'm **@MagSpotifyBot**.\nI'm Here to download your music.",
         reply_markup=InlineKeyboardMarkup(button),
     )
